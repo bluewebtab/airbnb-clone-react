0f60e1b4d05073f371e135b0874d375119330b8e
Ronny <ronny.icne@gmail.com>
2019-02-08 15:47:40 -0800

    Adds login and register page. Added authentication. Added auth service. Added Token interceptor and protected routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {Header} from './shared/Header';
+import Header from './components/shared/Header';
 import RentalListing from './components/rental/rental-listing/RentalListing';
 import RentalDetail from './components/rental/rental-detail/RentalDetail';
-
+import Login from './components/login/Login';
+import {Register} from './components/register/Register';
+import {ProtectedRoute} from './components/shared/auth/ProtectedRoute'
+import {LoggedInRoute} from './components/shared/auth/LoggedInRoute';
+import * as actions from 'actions';
 import './App.css';
 
 const store = require('./reducers').init()
@@ -12,23 +16,37 @@ const store = require('./reducers').init()
 
 class App extends Component {
   
+ componentWillMount(){
+   this.checkAuthState();
+ }
 
+ checkAuthState(){
+  store.dispatch(actions.checkAuthState())
+
+ }
+
+ logout(){
+   store.dispatch(actions.logout())
+ }
   
   render() {
     return (
       <Provider store={store}>
           <BrowserRouter>
             <div className="App">
-              <Header />
-              <div className='mainSection'>
+              <Header logout={this.logout} />
+              <section className='mainSection'>
                 <div className='mainpics container'>   
                   <Route exact path="/" render={() => {return <Redirect to='/rentals' /> }} />
                   <Route exact path="/rentals" component={RentalListing} />
-                  <Route exact path="/rentals/:id" component={RentalDetail} />
+                  <ProtectedRoute exact path="/rentals/:id" component={RentalDetail} />
+                  <Route exact path="/login" component={Login} />
+                  <LoggedInRoute exact path="/register" component={Register} />
+
                 </div>
-              </div>
+              </section>
             
-            </div>
+             </div>
           </BrowserRouter>
       </Provider>
     );
