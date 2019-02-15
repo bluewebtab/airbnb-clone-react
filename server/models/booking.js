517e4b2a535602251aeeb260d852cf0e6b765880
Ronny <ronny.icne@gmail.com>
2019-02-14 23:47:45 -0800

    Added booking creation functionality

--- server/models/booking.js
+++ server/models/booking.js
@@ -8,6 +8,7 @@ const bookingSchema = new Schema({
   days: Number,
   guests: Number,
   createdAt: {type: Date, default: Date.now},
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
   rental: {type: Schema.Types.ObjectId, ref: 'Rental'}
 });
 
