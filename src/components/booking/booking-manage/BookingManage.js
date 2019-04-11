2f090c23f85a2089cd4c603fe97876f3ee27956d
MyMac <ronny.icne@gmail.com>
2019-04-11 13:24:56 -0700

    Adds pre deploy setup

--- src/components/booking/booking-manage/BookingManage.js
+++ src/components/booking/booking-manage/BookingManage.js
@@ -1,45 +1,49 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {BookingCard} from './BookingCard';
-import * as actions from 'actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { BookingCard } from "./BookingCard";
+import * as actions from "actions";
 
 class BookingManage extends Component {
+  componentWillMount() {
+    this.props.dispatch(actions.fetchUserBookings());
+  }
 
-    componentWillMount(){
-        this.props.dispatch(actions.fetchUserBookings())
-    }
+  renderBookings(bookings) {
+    return bookings.map((booking, index) => (
+      <BookingCard booking={booking} key={index} />
+    ));
+  }
 
-    renderBookings(bookings){
-        return bookings.map((booking, index) =>
-            <BookingCard booking={booking} key={index} />
-        )
-    }
+  render() {
+    const { data: bookings, isFetching } = this.props.userBookings;
 
-    render(){
-        const {data: bookings, isFetching} = this.props.userBookings;
-
-        return (
-               <section id='userBookings'>
-                    <h1 className='page-title'>My Bookings</h1>
-                    <div className='row'>
-                    {this.renderBookings(bookings)}
-                    </div>
-                    { !isFetching && bookings.length === 0 && 
-                    <div class='alert alert-warning'>
-                        You have no bookings created go to rentals section and book your place today.
-                        <Link style={{'margin-left': '10px'}} class='btn btn-bwm' to='/rentals'>Available Rental</Link>
-                    </div>
-                    }
-                </section>
-        )
-    }
-}   
-
-function mapStateToProps(state){
-    return {
-        userBookings: state.userBookings
-    }
+    return (
+      <section id="userBookings">
+        <h1 className="page-title">My Bookings</h1>
+        <div className="row">{this.renderBookings(bookings)}</div>
+        {!isFetching && bookings.length === 0 && (
+          <div className="alert alert-warning">
+            You have no bookings created go to rentals section and book your
+            place today.
+            <Link
+              style={{ marginLeft: "10px" }}
+              className="btn btn-bwm"
+              to="/rentals"
+            >
+              Available Rental
+            </Link>
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
-export default connect(mapStateToProps)(BookingManage)
+function mapStateToProps(state) {
+  return {
+    userBookings: state.userBookings
+  };
+}
+
+export default connect(mapStateToProps)(BookingManage);
